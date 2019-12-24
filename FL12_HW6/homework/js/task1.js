e888e06eecd5f3015c601188f0b252d16cda9e3c
Roman Soroka <35731992+RomanSoroka@users.noreply.github.com>
2019-12-24 22:55:51 +0200

    Add files via upload

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,19 +1,22 @@
 let a = +prompt('Enter a', '');
 let b = +prompt('Enter b', '');
 let c = +prompt('Enter c', '');
+const FOUR = 4;
+const TWO = 2;
+const TEN = 10;
 
 if (isNaN(a) || isNaN(b) || isNaN(c) || a === 0) {
    alert('Invalid input data');
 } else {
-    let d = b * b - 4 * a * c;
+    let d = b * b - FOUR * a * c;
     if (d < 0) {
         alert('No solutin, D < 0');
     } else if (d === 0) {
-        let x = -b / 2 * a;
+        let x = -b / TWO * a;
         alert('x = ' + x);
     } else {
-        let x1 = (-b + Math.sqrt(d)) / (2 * a);
-        let x2 = (-b - Math.sqrt(d)) / (2 * a);
-        alert('x1 = ' + Math.floor(x1 *10)/10 + ';' + ' x2 = ' + Math.floor(x2 *10)/10);
+        let x1 = (-b + Math.sqrt(d)) / (TWO * a);
+        let x2 = (-b - Math.sqrt(d)) / (TWO * a);
+        alert('x1 = ' + Math.floor(x1 * TEN) / TEN + ';' + ' x2 = ' + Math.floor(x2 * TEN) / TEN);
     }
 }
