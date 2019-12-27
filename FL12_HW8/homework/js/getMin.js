bad4ab121ea2965c1506d18bcf5bb2269d933955
Roman Soroka <35731992+RomanSoroka@users.noreply.github.com>
2019-12-27 14:49:43 +0200

    Update getMin.js

--- FL12_HW8/homework/js/getMin.js
+++ FL12_HW8/homework/js/getMin.js
@@ -1,6 +1,6 @@
 function getMin(){
-	let min = arguments[0];
-	for (let i = 1; i < arguments.length; i++){
+  let min = arguments[0];
+  for (let i = 1; i < arguments.length; i++){
     if (arguments[i] < min){
       min = arguments[i];
     }
