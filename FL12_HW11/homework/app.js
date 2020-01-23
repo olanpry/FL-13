26d958c98b5520c2f8d196635c94518be9271876
Roman Soroka <35731992+RomanSoroka@users.noreply.github.com>
2020-01-23 13:11:40 +0200

    Add files via upload

--- FL12_HW11/homework/app.js
+++ FL12_HW11/homework/app.js
@@ -1,81 +1,79 @@
-const structure = [
-  {
-    folder: true,
-    title: 'Films',
-    children: [
-      {
-        title: 'Iron Man.avi'
-      },
-      {
+const structure = [{
         folder: true,
-        title: 'Fantasy',
-        children: [
-          {
-            title: 'The Lord of the Rings.avi'
-          },
-          {
-            folder: true,
-            title: 'New folder 1',
-            children: false
-          }
+        title: 'Films',
+        children: [{
+                title: 'Iron Man.avi'
+            },
+            {
+                folder: true,
+                title: 'Fantasy',
+                children: [{
+                        title: 'The Lord of the Rings.avi'
+                    },
+                    {
+                        folder: true,
+                        title: 'New folder 1',
+                        children: false
+                    }
+                ]
+            }
         ]
-      }
-    ]
-  },
-  {
-    folder: true,
-    title: 'Documents',
-    children: [
-      {
+    },
+    {
         folder: true,
-        title: 'EPAM Homework answers',
-        children: null
-      }
-    ]
-  }
+        title: 'Documents',
+        children: [{
+            folder: true,
+            title: 'EPAM Homework answers',
+            children: null
+        }]
+    }
 ];
 
 const rootNode = document.getElementById('root');
 
 function createTree(container, data) {
-  let ul = document.createElement('ul');
-  for (let obj of data) {
-    let li = document.createElement('li');
-    if (obj.folder) {
-      li.className = 'folder';
-      li.innerHTML = '<i class="material-icons folder_icon" id="open">folder</i>' + obj.title;
-    } else {
-      li.innerHTML = '<i class="material-icons insert_drive_file">insert_drive_file</i>' + obj.title;
+    let ul = document.createElement('ul');
+    for (let obj of data) {
+        let li = document.createElement('li');
+        if (obj.folder) {
+            li.className = 'folder';
+            li.innerHTML = '<i class="material-icons folder_icon" id="open">folder</i>' + obj.title;
+        } else {
+            li.innerHTML = '<i class="material-icons insert_drive_file">insert_drive_file</i>' + obj.title;
+        }
+        ul.append(li);
+        if (obj.children) {
+            createTree(ul, obj.children);
+        }
+        if (obj.folder === true && !obj.children) {
+            let p = document.createElement('p');
+            p.className = 'closed';
+            p.innerHTML = '<i>This folder is empty!</i>';
+            ul.append(p);
+        }
     }
-    ul.append(li);
-    if (obj.children) {
-      createTree(ul, obj.children);
+    container.append(ul);
+    if (ul.parentElement.tagName === 'UL') {
+        ul.classList.toggle('closed');
     }
-    if (obj.folder === true && !obj.children) {
-      let p = document.createElement('p');
-      p.className = 'closed';
-      p.innerHTML = '<i>This folder is empty!</i>';
-      ul.append(p);
-    }
-  }
-  container.append(ul);
-  if(ul.parentElement.tagName === 'UL'){
-    ul.classList.toggle('closed');
-  }
 }
 
 createTree(rootNode, structure);
 
 let elem = document.getElementsByClassName('folder');
 for (let i = 0; i < elem.length; i++) {
-  elem[i].addEventListener('click', openFolder);
+    elem[i].addEventListener('click', openFolder);
 }
+
 function openFolder() {
-  event.target.nextSibling.classList.remove('closed');
-  event.target.nextSibling.classList.toggle('open');
-  event.target.firstChild.innerHTML = 'folder_open';
-  if (!event.target.nextSibling.classList.contains('open')) {
-    event.target.nextSibling.classList.toggle('closed');
-    event.target.firstChild.innerHTML = 'folder';
-  }
+    if (event.target.nextSibling.classList.contains('closed')) {
+        event.target.nextSibling.classList.remove('closed');
+        event.target.nextSibling.classList.toggle('open');
+        event.target.firstChild.innerHTML = 'folder_open';
+    } else {
+        event.target.nextSibling.classList.remove('open');
+        event.target.nextSibling.classList.toggle('closed');
+        event.target.firstChild.innerHTML = 'folder';
+    }
 }
