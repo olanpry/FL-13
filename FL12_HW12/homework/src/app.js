2192634ceda09140f8a6c0875bd32e72c6e7bbad
Roman Soroka <35731992+RomanSoroka@users.noreply.github.com>
2020-01-27 00:07:50 +0200

    Add files via upload

--- FL12_HW12/homework/src/app.js
+++ FL12_HW12/homework/src/app.js
@@ -1,4 +1,78 @@
 let rootNode = document.getElementById('root');
+let h1 = document.createElement('h1');
+let mainPage = document.createElement('div');
+let addNewSetPage = document.createElement('div');
+let modifyExistSetPage = document.createElement('div');
+
+let addNewButton = document.createElement('button');
+let flashcardContainer = document.createElement('div');
+let flashcard = document.createElement('div');
+
+let nameInput = document.createElement('input');
+let addTermsBtn = document.createElement('button');
+let saveBtn = document.createElement('button');
+let cancelBtn = document.createElement('button');
+let termContainer = document.createElement('div');
+let term = document.createElement('input');
+let definition = document.createElement('input');
+
+let newSave = document.createElement('button');
+let newCancel = document.createElement('button');
+let editName = document.createElement('input');
+let editTerm = document.createElement('input');
+let editDef = document.createElement('input');
+
+let id = 1;
+
+rootNode.append(h1);
+rootNode.append(mainPage);
+rootNode.append(addNewSetPage);
+rootNode.append(modifyExistSetPage);
+
+mainPage.append(addNewButton);
+mainPage.append(flashcardContainer);
+
+addNewSetPage.append(nameInput);
+addNewSetPage.append(addTermsBtn);
+addNewSetPage.append(saveBtn);
+addNewSetPage.append(cancelBtn);
+addNewSetPage.append(termContainer);
+termContainer.append(term);
+termContainer.append(definition);
+
+modifyExistSetPage.append(newSave);
+modifyExistSetPage.append(newCancel);
+modifyExistSetPage.append(editName);
+modifyExistSetPage.append(editTerm);
+modifyExistSetPage.append(editDef);
+
+h1.innerHTML = 'Term Constructor v1.0.1';
+mainPage.classList.add('main-page');
+addNewSetPage.classList.add('new-set-page', 'hide');
+modifyExistSetPage.classList.add('modify-page', 'hide');
+
+addNewButton.innerHTML = 'Add new';
+flashcardContainer.classList.add('flashcard-conatiner');
+flashcard.classList.add('flashcard');
+
+nameInput.setAttribute('type', 'text');
+nameInput.setAttribute('placeholder', 'Name');
+addTermsBtn.innerHTML = 'Add Terms';
+saveBtn.innerHTML = 'Save';
+cancelBtn.innerHTML = 'Cancel';
+cancelBtn.insertAdjacentHTML('afterend', '<br>');
+termContainer.classList.add('term-container', 'hide');
+term.setAttribute('type', 'text');
+term.setAttribute('placeholder', 'Term');
+term.insertAdjacentHTML('afterend', '<br>');
+definition.setAttribute('type', 'text');
+definition.setAttribute('placeholder', 'Definition');
+
+newCancel.innerHTML = 'Cancel';
+newSave.innerHTML = 'Save New';
+editName.setAttribute('type', 'text');
+editTerm.setAttribute('type', 'text');
+editDef.setAttribute('type', 'text');
 
 const MAIN_PAGE_HASH = '/mainpage';
 const ADD_NEW_HASH = '/addnew';
@@ -8,193 +82,95 @@ window.onload = function() {
   location.hash = MAIN_PAGE_HASH;
 };
 
-let h1 = document.createElement('h1');
-h1.innerHTML = 'Term Constructor v1.0.1';
-rootNode.append(h1);
-
-let mainPage = document.createElement('div');
-mainPage.classList.add('main-page');
-let addNewSetPage = document.createElement('div');
-addNewSetPage.classList.add('new-set-page', 'hide');
-let modifyExistSetPage = document.createElement('div');
-modifyExistSetPage.classList.add('modify-page', 'hide');
-
-let newSave = document.createElement('button');
-newSave.innerHTML = 'Save New';
-let newCancel = document.createElement('button');
-newCancel.innerHTML = 'Cancel';
-modifyExistSetPage.append(newSave);
-modifyExistSetPage.append(newCancel);
-
-rootNode.append(mainPage);
-rootNode.append(addNewSetPage);
-rootNode.append(modifyExistSetPage);
-
-let addNewButton = document.createElement('button');
-addNewButton.innerHTML = 'Add new';
-mainPage.append(addNewButton);
-
-let flashcardContainer = document.createElement('div');
-flashcardContainer.classList.add('flashcard-conatiner');
-
 addNewButton.onclick = function() {
-  mainPage.append(flashcardContainer);
   location.hash = ADD_NEW_HASH;
 };
-
-let nameInput = document.createElement('input');
-nameInput.setAttribute('type', 'text');
-nameInput.setAttribute('placeholder', 'Name');
-
-let addTermsBtn = document.createElement('button');
-addTermsBtn.innerHTML = 'Add Terms';
-
-let saveBtn = document.createElement('button');
-saveBtn.innerHTML = 'Save';
-
-let cancelBtn = document.createElement('button');
-cancelBtn.innerHTML = 'Cancel';
-
-addNewSetPage.append(nameInput);
-addNewSetPage.append(addTermsBtn);
-addNewSetPage.append(saveBtn);
-addNewSetPage.append(cancelBtn);
-
-cancelBtn.insertAdjacentHTML('afterend', '<br>');
-
-let termContainer = document.createElement('div');
-termContainer.classList.add('term-container', 'hide');
-addNewSetPage.append(termContainer);
-
-let term = document.createElement('input');
-let definition = document.createElement('input');
-term.setAttribute('type', 'text');
-term.setAttribute('placeholder', 'Term');
-definition.setAttribute('type', 'text');
-definition.setAttribute('placeholder', 'Definition');
-termContainer.append(term);
-term.insertAdjacentHTML('afterend', '<br>');
-termContainer.append(definition);
 
 addTermsBtn.onclick = function() {
   termContainer.classList.toggle('hide');
 };
 
-let id = 1;
-
 saveBtn.onclick = function() {
   if (!nameInput.value) {
     alert('Enter Name');
   } else {
-    let flashcard = document.createElement('div');
-    flashcard.classList.add('flashcard');
-    flashcard.setAttribute('id', `${id++}`);
-    flashcard.innerHTML = '<p class="name-input">' + nameInput.value + '</p>';
-    if (term.value) {
-      flashcard.innerHTML += '<p class="term-input">' + term.value + '</p>';
-    }
-    if (definition.value) {
-      flashcard.innerHTML +=
-        '<p class="definition-input">' + definition.value + '</p>';
-    }
-
-    if (document.querySelector('.done')) {
-      document.querySelector('.done').before(flashcard);
-    } else {
-      flashcardContainer.append(flashcard);
-    }
-
-    nameInput.value = '';
-    term.value = '';
-    definition.value = '';
-    if (termContainer) {
-      termContainer.classList.add('hide');
-    }
-
+    flashcardGenerator();
+    reset();
     location.hash = MAIN_PAGE_HASH;
-
-    let markbtn = document.createElement('button');
-    markbtn.innerHTML = 'Done';
-    let remove = document.createElement('button');
-    remove.innerHTML = 'Remove';
-    let edit = document.createElement('button');
-    edit.innerHTML = 'Edit';
-    let btncontainer = document.createElement('div');
-    btncontainer.append(markbtn);
-    btncontainer.append(edit);
-    btncontainer.append(remove);
-    flashcard.append(btncontainer);
-
-    remove.onclick = function() {
-      flashcardContainer.removeChild(flashcard);
-    };
-
-    markbtn.onclick = function() {
-      flashcardContainer.append(flashcard);
-      termContainer.classList.add('hide');
-      flashcard.classList.add('done');
-    };
-
-    edit.onclick = function() {
-      location.hash = MOD_PAGE_HASH + id;
-      let target = event.target.parentNode.parentNode;
-      let editName = document.createElement('input');
-      editName.setAttribute('type', 'text');
-      let editTerm = document.createElement('input');
-      editTerm.setAttribute('type', 'text');
-      let editDef = document.createElement('input');
-      editDef.setAttribute('type', 'text');
-      editName.value = target.querySelector('.name-input').innerHTML;
-      modifyExistSetPage.append(editName);
-      newSave.addEventListener('click', newsave);
-      newCancel.onclick = function() {
-        location.hash = MAIN_PAGE_HASH;
-        modifyExistSetPage.removeChild(editName);
-        modifyExistSetPage.removeChild(editTerm);
-        modifyExistSetPage.removeChild(editDef);
-      };
-      if (target.querySelector('.term-input')) {
-        editTerm.value = target.querySelector('.term-input').innerHTML;
-        modifyExistSetPage.append(editTerm);
-      } else {
-        modifyExistSetPage.removeChild(editTerm);
-      }
-      if (target.querySelector('.definition-input')) {
-        editDef.value = target.querySelector('.definition-input').innerHTML;
-        modifyExistSetPage.append(editDef);
-      } else {
-        modifyExistSetPage.removeChild(editDef);
-      }
-      function newsave() {
-        if (!editName.value) {
-          alert('Enter name');
-        } else {
-          target.querySelector('.name-input').innerHTML = editName.value;
-        }
-        if (editTerm.value) {
-          target.querySelector('.term-input').innerHTML = editTerm.value;
-        }
-        if (editDef.value) {
-          target.querySelector('.definition-input').innerHTML = editDef.value;
-        }
-        location.hash = MAIN_PAGE_HASH;
-        modifyExistSetPage.removeChild(editName);
-        modifyExistSetPage.removeChild(editTerm);
-        modifyExistSetPage.removeChild(editDef);
-      }
-    };
   }
 };
 
 cancelBtn.onclick = function() {
+  reset();
+  location.hash = MAIN_PAGE_HASH;
+};
+
+function flashcardGenerator() {
+  let card = flashcard.cloneNode(true);
+  card.setAttribute('id', `${id++}`);
+  card.innerHTML = '<p class="name-input">' + nameInput.value + '</p>';
+  card.innerHTML += '<p class="term-input">' + term.value + '</p>';
+  card.innerHTML += '<p class="definition-input">' + definition.value + '</p>';
+  btnGenerator(card);
+  if (document.querySelector('.done')) {
+    document.querySelector('.done').before(card);
+  } else {
+    flashcardContainer.append(card);
+  }
+}
+
+function btnGenerator(card) {
+  let markbtn = document.createElement('button');
+  let remove = document.createElement('button');
+  let edit = document.createElement('button');
+  markbtn.innerHTML = 'Done';
+  edit.innerHTML = 'Edit';
+  remove.innerHTML = 'Remove';
+  let btncontainer = document.createElement('div');
+  btncontainer.append(markbtn);
+  btncontainer.append(edit);
+  btncontainer.append(remove);
+  card.append(btncontainer);
+
+  remove.onclick = function() {
+    flashcardContainer.removeChild(card);
+  };
+  markbtn.onclick = function() {
+    flashcardContainer.append(card);
+    card.classList.add('done');
+  };
+
+  edit.onclick = function() {
+    let target = event.target.parentNode.parentNode;
+    location.hash = MOD_PAGE_HASH + id;
+    editName.value = target.querySelector('.name-input').innerHTML;
+    editTerm.value = target.querySelector('.term-input').innerHTML;
+    editDef.value = target.querySelector('.definition-input').innerHTML;
+    newSave.onclick = function() {
+      if (!editName.value) {
+        alert('Enter Name');
+      } else {
+        target.querySelector('.name-input').innerHTML = editName.value;
+        location.hash = MAIN_PAGE_HASH;
+      }
+      target.querySelector('.term-input').innerHTML = editTerm.value;
+      target.querySelector('.definition-input').innerHTML = editDef.value;
+    };
+  };
+}
+
+newCancel.onclick = function() {
+  location.hash = MAIN_PAGE_HASH;
+};
+
+function reset() {
   nameInput.value = '';
   term.value = '';
   definition.value = '';
   if (termContainer) {
     termContainer.classList.add('hide');
   }
-  location.hash = MAIN_PAGE_HASH;
-};
+}
 
 window.addEventListener('hashchange', function() {
   switch (location.hash.split('/')[1]) {
